b7483a70abce6f7e067e8fbfa6e9f7c7844550d8
UTKARSH <129024073+KING-UTKARSH@users.noreply.github.com>
2024-08-13 23:41:23 +0530

    Add files via upload

--- Unity/Structures/System_String.hpp
+++ Unity/Structures/System_String.hpp
@@ -1,30 +1,46 @@
 #pragma once
 
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wtautological-undefined-compare"
+#pragma clang diagnostic ignored "-Wtautological-pointer-compare"
+#endif
+
 namespace Unity
 {
     struct System_String : il2cppObject
     {
-        int m_iLength;
-        wchar_t m_wString[1024];
-
+        int32_t m_iLength;       // 0x8
+        wchar_t m_wString[1024]; // 0xC
+        
         void Clear()
         {
-            if (!this) return;
+            if (this == nullptr) return;
             
             memset(m_wString, 0, static_cast<size_t>(m_iLength) * 2);
             m_iLength = 0;
         }
         
+        wchar_t* ToWideString()
+        {
+            return m_wString;
+        }
+        
+        int32_t ToLength()
+        {
+            return m_iLength;
+        }
+        
         std::string ToString()
         {
-            if (!this || m_wString == nullptr || m_iLength == 0)
+            if ((this == nullptr) || (m_wString == nullptr) || (m_iLength == 0))
                 return "";
             
             std::string retStr;
             mbstate_t state = mbstate_t();
             char buf[MB_CUR_MAX];
             
-            for (size_t i = 0; i < m_iLength; ++i)
+            for (int32_t i = 0; i < m_iLength; ++i)
             {
                 size_t ret = wcrtomb(buf, m_wString[i], &state);
                 if (ret == (size_t)-1)
@@ -37,3 +53,7 @@ namespace Unity
         }
     };
 }
+
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#endif
