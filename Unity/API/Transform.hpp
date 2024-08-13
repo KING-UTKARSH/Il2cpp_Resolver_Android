e5eb5886403c57f99ab4b129e6c1b6707c1ed36b
UTKARSH <129024073+KING-UTKARSH@users.noreply.github.com>
2024-08-13 23:42:48 +0530

    Add files via upload

--- Unity/API/Transform.hpp
+++ Unity/API/Transform.hpp
@@ -10,10 +10,12 @@ namespace Unity
         void* m_GetChildCount = nullptr;
         void* m_FindChild = nullptr;
         void* m_GetPosition = nullptr;
+        void* m_GetForward = nullptr;
         void* m_GetRotation = nullptr;
         void* m_GetLocalPosition = nullptr;
         void* m_GetLocalScale = nullptr;
         void* m_SetPosition = nullptr;
+        void* m_SetForward = nullptr;
         void* m_SetRotation = nullptr;
         void* m_SetLocalPosition = nullptr;
         void* m_SetLocalScale = nullptr;
@@ -63,7 +65,14 @@ namespace Unity
             reinterpret_cast<void(UNITY_CALLING_CONVENTION)(void*, Vector3&)>(m_TransformFunctions.m_GetPosition)(this, vRet);
             return vRet;
         }
-
+        
+        Vector3 GetForward()
+        {
+            Vector3 vRet;
+            vRet = reinterpret_cast<Vector3(UNITY_CALLING_CONVENTION)(void*)>(m_TransformFunctions.m_GetForward)(this);
+            return vRet;
+        }
+        
         Quaternion GetRotation()
         {
             Quaternion m_qRet;
@@ -89,7 +98,12 @@ namespace Unity
         {
             reinterpret_cast<void(UNITY_CALLING_CONVENTION)(void*, Vector3)>(m_TransformFunctions.m_SetPosition)(this, m_vVector);
         }
-
+        
+        void SetForward(Vector3 m_vVector)
+        {
+            reinterpret_cast<void(UNITY_CALLING_CONVENTION)(void*, Vector3)>(m_TransformFunctions.m_SetForward)(this, m_vVector);
+        }
+        
         void SetRotation(Quaternion m_qQuat)
         {
             reinterpret_cast<void(UNITY_CALLING_CONVENTION)(void*, Quaternion)>(m_TransformFunctions.m_SetRotation)(this, m_qQuat);
@@ -111,17 +125,19 @@ namespace Unity
         void Initialize()
         {
             IL2CPP::SystemTypeCache::Initializer::Add(UNITY_TRANSFORM_CLASS);
-
+                
             m_TransformFunctions.m_GetParent        = IL2CPP::ResolveCall(UNITY_TRANSFORM_GETPARENT);
             m_TransformFunctions.m_GetRoot            = IL2CPP::ResolveCall(UNITY_TRANSFORM_GETROOT);
             m_TransformFunctions.m_GetChild            = IL2CPP::ResolveCall(UNITY_TRANSFORM_GETCHILD);
             m_TransformFunctions.m_GetChildCount    = IL2CPP::ResolveCall(UNITY_TRANSFORM_GETCHILDCOUNT);
             m_TransformFunctions.m_FindChild        = IL2CPP::ResolveCall(UNITY_TRANSFORM_FINDCHILD);
             m_TransformFunctions.m_GetPosition        = IL2CPP::ResolveCall(UNITY_TRANSFORM_GETPOSITION);
+            m_TransformFunctions.m_GetForward        = IL2CPP::ResolveCall(UNITY_TRANSFORM_GETFORWARD);
             m_TransformFunctions.m_GetRotation        = IL2CPP::ResolveCall(UNITY_TRANSFORM_GETROTATION);
             m_TransformFunctions.m_GetLocalPosition = IL2CPP::ResolveCall(UNITY_TRANSFORM_GETLOCALPOSITION);
             m_TransformFunctions.m_GetLocalScale    = IL2CPP::ResolveCall(UNITY_TRANSFORM_GETLOCALSCALE);
             m_TransformFunctions.m_SetPosition        = IL2CPP::ResolveCall(UNITY_TRANSFORM_SETPOSITION);
+            m_TransformFunctions.m_SetForward        = IL2CPP::ResolveCall(UNITY_TRANSFORM_SETFORWARD);
             m_TransformFunctions.m_SetRotation        = IL2CPP::ResolveCall(UNITY_TRANSFORM_SETROTATION);
             m_TransformFunctions.m_SetLocalPosition = IL2CPP::ResolveCall(UNITY_TRANSFORM_SETLOCALPOSITION);
             m_TransformFunctions.m_SetLocalScale    = IL2CPP::ResolveCall(UNITY_TRANSFORM_SETLOCALSCALE);
